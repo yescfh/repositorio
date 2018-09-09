2d2002ccaf0d22a83a78f12c5422031ec3c73479
yrodr <yrodr@DESKTOP-S6OHGKQ>
2018-09-09 10:38:51 -0300

    Probando el primer repositorio

---
+++ src/area/triangulo/AreaTriangulo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package area.triangulo;
+
+/**
+ *
+ * @author yrodr
+ */
+public class AreaTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double base=5;
+        double altura=3;
+        double area= (base*altura)/2;
+        
+        System.out.println ("Inserte base");
+        System.out.println(base);
+        System.out.println ("Inserte altura");
+        System.out.println(altura);
+        System.out.println ("El area del triangulo es");
+        System.out.println(area);
+          
+    }
+    
+}
